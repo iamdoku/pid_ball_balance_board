72f38ff8525bbf33f7c7df584ca6775ee4d697d4
iamdoku <adam.uchytil@gmail.com>
2019-04-19 19:22:04 +0200

    commit

--- source/ball_tracking.cpp
+++ source/ball_tracking.cpp
@@ -1,21 +1,56 @@
-#include <iostream>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <stdexcept>
 #include <wiringSerial.h>
-#include <cmath>
 
 const double PI = 3.14159265;
+
+class PID {
+public:
+  PID(double p, double i, double d) : kp(p), ki(i), kd(d) {}
+
+  double calculateError(const double &w, const double &y) {
+    e = w - y;
+    return e;
+  }
+
+  double regulate(const double &w, const double &y) {
+    double prev_e = e;
+    calculateError(w, y);
+    ie += e * (1 / 30);
+    return kp * e + ki * ie + kd * ((e - prev_e) / (1 / 30));
+  }
+
+private:
+  double kp, ki, kd;
+  double e = 0, ie = 0;
+};
+
+double calculateAngle(const cv::Vec2d &v) {
+  if (v[0] > 0 && v[1] > 0) {
+    return atan(v[1] / v[0]);
+  } else if (v[0] < 0 && v[1] > 0) {
+    return atan(v[1] / -v[0]) + (PI / 2);
+  } else if (v[0] < 0 && v[1] < 0) {
+    return atan(v[1] / v[0]) + PI;
+  } else if (v[0] > 0 && v[1] < 0) {
+    return atan(-v[1] / v[0]) + ((3 / 2) * PI);
+  } else
+    return 0;
+}
 
 class FilterHSV {
 public:
   FilterHSV(std::ifstream &is, cv::VideoCapture &stream) : vc(stream) {
     is >> low_H >> low_S >> low_V >> high_H >> high_S >> high_V;
   }
-  FilterHSV(int lh, int ls, int lv, int hh, int hs, int hv, cv::VideoCapture &stream)
-      : low_H(lh), low_S(ls), low_V(lv), high_H(hh), high_S(hs), high_V(hv), vc(stream) {}
+  FilterHSV(int lh, int ls, int lv, int hh, int hs, int hv,
+            cv::VideoCapture &stream)
+      : low_H(lh), low_S(ls), low_V(lv), high_H(hh), high_S(hs), high_V(hv),
+        vc(stream) {}
   FilterHSV() = default;
 
   cv::Scalar retLow() { return cv::Scalar(low_H, low_S, low_V); }
@@ -64,14 +99,14 @@ public:
     return hsv_frame;
   }
 
-  cv::Point2d getCenter(){
+  cv::Point2d getCenter() {
     cv::Moments m = cv::moments(filter(), true);
     return cv::Point2d(m.m10 / m.m00, m.m01 / m.m00);
   }
 
-  double getRadius(){
+  double getRadius() {
     cv::Moments m = cv::moments(filter(), true);
-    return sqrt(m.m00/PI);
+    return sqrt(m.m00 / PI);
   }
 
 private:
@@ -81,9 +116,8 @@ private:
   int low_V = 0, high_V = 255;
 };
 
-
-int main(){
-    int fd = serialOpen("/dev/ttyUSB0", 9600);
+int main() {
+  int fd = serialOpen("/dev/ttyUSB0", 9600);
   if (fd < 0)
     return -1;
 
@@ -100,7 +134,6 @@ int main(){
   ball.calibrate(camera);
   board.calibrate(camera);
 
-
   cv::Point2d center_board = board.getCenter();
   double radius = board.getRadius();
 
@@ -115,7 +148,6 @@ int main(){
     frame_filtered_ball = ball.filter(frame_bgr);
     frame_filtered_board = board.filter(frame_bgr);
 
-
     cv::Moments m_ball = cv::moments(frame_filtered_ball, true);
 
     cv::Point2d center_ball(m_ball.m10 / m_ball.m00, m_ball.m01 / m_ball.m00);
@@ -123,10 +155,10 @@ int main(){
     cv::Point2d diff = center_ball - center_board;
     cv::Vec2d ball_vec(diff);
 
-    double amplitude = sqrt(pow(ball_vec[0],2)+pow(ball_vec[1],2));
-    double angle = std::atan(ball_vec[1]/ball_vec[0]) * 180/PI;
+    double amplitude = sqrt(pow(ball_vec[0], 2) + pow(ball_vec[1], 2));
+    double angle = calculateAngle(ball_vec) * 180 / PI;
 
-    std::cout << (amplitude/radius)*100 << " " << angle << std::endl;
+    std::cout << (amplitude / radius) * 100 << " " << angle << std::endl;
 
     cv::circle(frame_bgr, center_ball, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
     cv::circle(frame_bgr, center_board, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
