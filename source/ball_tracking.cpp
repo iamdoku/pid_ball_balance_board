d045383f6377b18b3c608fd857b94193b2e172f5
iamdoku <adam.uchytil@gmail.com>
2019-04-20 19:11:58 +0200

    commit

--- source/ball_tracking.cpp
+++ source/ball_tracking.cpp
@@ -167,21 +167,24 @@ int main() {
     double amplitude = sqrt(pow(ball_vec[0], 2) + pow(ball_vec[1], 2));
     double angle = calculateAngle(ball_vec);
     //int x = 180 - (std::trunc(((1 * regulator.calculateError(0, amplitude) * 90) / radius))+90);
-    int x = 2000 + std::trunc(3 * regulator.regulate(0, amplitude) * 500 / radius);
+    int x = std::trunc(3 * regulator.regulate(0, amplitude) * 500 / radius);
     std::cout << x << std::endl;
     //std::cout << x << std::endl;
 
     if (angle <= (2.0 / 3.0) * PI){
-      a_level = x;
-      c_level = x;
+      a_level = 2000 + x;
+      c_level = 2000 + x;
+      b_level = 1500 - x;
     }
     else if (angle > (2.0 / 3.0) * PI && angle <= (4.0 / 3.0) * PI){
-      a_level = x;
-      b_level = x;
+      a_level = 2000 + x;
+      b_level = 2000 + x;
+      c_level = 1500 - x;
     }
     else if (angle > (4.0 / 3.0) * PI && angle <= 2 * PI){
-      b_level = x;
-      c_level = x;
+      b_level = 2000 + x;
+      c_level = 2000 + x;
+      a_level = 1500 - x;
     }
 
     std::stringstream ss_a, ss_b, ss_c;
